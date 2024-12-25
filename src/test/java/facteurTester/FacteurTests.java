3a23ef91ea10e6ef27e80e51d47c7030d4808552
issam-mustapha <issamadoch777@gmail.com>
2024-12-25 17:18:35 +0100

    cycle 2' tester 2

--- src/test/java/facteurTester/FacteurTests.java
+++ src/test/java/facteurTester/FacteurTests.java
@@ -2,19 +2,40 @@ package facteurTester;
 import java.util.ArrayList;
 import java.util.List;
 import facteursPremier.FacteurPremier;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.stream.DoubleStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FacteurTests {
+    List<Integer> expected;
+    List<Integer> actual_veleur;
+    @BeforeEach
+    void setUp(){
+        expected=new ArrayList<Integer>();
+        actual_veleur=new ArrayList<Integer>();
+    }
+    @AfterEach
+    void tearDown(){
+        expected=null;
+        actual_veleur=null;
+    }
     @Test
     public  void test_1_devrait_retourne_liste_vide(){
         //given
-    List<Integer> expected= new ArrayList<Integer>();
+
        //when
-    List<Integer> actual_veleur=FacteurPremier.generate(1);
+     actual_veleur=FacteurPremier.generate(1);
         //then
      assertThat(actual_veleur).isEqualTo(expected);
+    }
+    @Test
+    public void test_2_devrait_retourne_2(){
+        expected.add(2);
+        actual_veleur=FacteurPremier.generate(2);
+        assertThat(actual_veleur).isEqualTo(expected);
     }
 }
